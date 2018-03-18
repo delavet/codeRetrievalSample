c2c922cd290ac993195f5949bed24f84cef6aa85
delavet <yhang1996@126.com>
2018-03-18 21:26:30 +0800

    Update topic_num_detect.py

--- topic_num_detect.py
+++ topic_num_detect.py
@@ -52,7 +52,7 @@ def test_topic_num(topic_num):
     measure_file.write(write_str)
 
 
-numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70]
+numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150]
 for num in numbers:
     test_topic_num(num)
 measure_file.close()
